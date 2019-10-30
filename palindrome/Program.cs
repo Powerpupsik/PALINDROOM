3b6aa1b2ff61cb90f6bdde13be5d05ee03c8843a
Powerpupsik <raima.levistu@gmail.com>
2019-10-30 21:20:53 +0200

    p

---
+++ palindrome/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter your word here: ");
+            string word = Console.ReadLine();
+            string reverse;
+            char[] rev = word.ToCharArray();
+
+            Array.Reverse(rev);
+            reverse = new string(rev);
+
+            bool a = word.Equals(reverse, StringComparison.OrdinalIgnoreCase);
+            if( a  == true  )
+            {
+                Console.WriteLine($"{word} is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Your word is not palindrome");
+            }
+
+            Console.ReadLine();
+        }
+       
+
+        
+        
+
+            
+        
+
+    }
+}
